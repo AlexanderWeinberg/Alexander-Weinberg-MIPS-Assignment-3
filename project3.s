4feadfa47a7455a812b540d2a3a54928f9c15210
AlexanderWeinberg <alexw.dagreat@gmail.com>
2019-11-19 15:36:53 -0500

    Initial Commit to Github

---
+++ project3.s
@@ -0,0 +1,14 @@
+#My ID: 02876360 %11 = 3 +26 = Base 29
+
+.data
+user_input: .space 1000	   #makes 1000 spaces for the user input
+endl: .asciiz "\n"	   #makes asciiz character for a new line
+invalid_msg: .asciiz "Invalid input"  #makes asciiz Invalid message
+
+.text
+
+main:
+
+
+
+
